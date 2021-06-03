9181fed68d96bb5495851c21e068d071fd192f06
Kingsley Allen <kingsley.allen@gmail.com>
2021-06-02 23:27:32 -0500

    Getting tests set up

---
+++ client/overview/overview.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Overview extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // not yet
+    }
+  }
+
+  render() {
+    return() {
+      null;
+    }
+  }
+
+
+
+
+}
