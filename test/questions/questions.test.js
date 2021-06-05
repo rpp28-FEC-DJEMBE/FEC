616e8e541d3dea439a10a374d9232b072a46cd65
Simon Si <simonsi@outlook.com>
2021-06-05 12:38:19 -0700

    qatest

--- test/questions/questions.test.js
+++ test/questions/questions.test.js
@@ -3,14 +3,13 @@ import {shallow, configure} from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 configure({adapter: new Adapter()});
 
-import Overview from '../../client/questions/questions.jsx';
+import Questions from '../../client/questions/questions.jsx';
 
 describe('Test Overview component', () => {
 
   test("Renders component correctly", () => {
-    const wrapper = shallow(<Overview />)
+    const wrapper = shallow(<Questions />)
     expect(wrapper.exists()).toBe(true);
-    expect(wrapper.find(".product-image").text()).toEqual("Product image component");
   })
 
 });
