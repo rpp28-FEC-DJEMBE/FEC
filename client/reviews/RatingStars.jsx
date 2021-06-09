f9195e90cd539dd601a92cb7a98066c18801945c
Andre Boussarath <andreboussarath@gmail.com>
2021-06-09 09:43:17 -0500

    removed pseudocode

--- client/reviews/RatingStars.jsx
+++ client/reviews/RatingStars.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 
 const starStyle = {
-  // --percent: calc(var(--rating) / 5 * 100%);
-
   display: 'inline-block',
   fontFamily: 'Times',
   lineHeight: 1,
@@ -13,11 +11,7 @@ const percent= { '--percent': '50%' };
 const RatingStars = (props) => {
 
   return (
-
-
-
     <div className='stars' style={{...starStyle, ...percent}}></div>
-    // <div className='stars' ></div>
   )
 }
 
