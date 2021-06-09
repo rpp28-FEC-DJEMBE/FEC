78fc9fe90b312af7effd2ee4a574dd7d7a7fb7b9
Andre Boussarath <andreboussarath@gmail.com>
2021-06-09 09:39:16 -0500

    Stars rendering with static data

--- client/reviews/RatingStars.jsx
+++ client/reviews/RatingStars.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 
-const RatingStars = () => {
+const starStyle = {
+  // --percent: calc(var(--rating) / 5 * 100%);
+
+  display: 'inline-block',
+  fontFamily: 'Times',
+  lineHeight: 1,
+}
+
+const percent= { '--percent': '50%' };
+
+const RatingStars = (props) => {
+
   return (
-    <div className='stars' style={--rating: 2.3}></div>
+
+
+
+    <div className='stars' style={{...starStyle, ...percent}}></div>
+    // <div className='stars' ></div>
   )
 }
 
---
+++ server_utils/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  TOKEN: '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'
+};
