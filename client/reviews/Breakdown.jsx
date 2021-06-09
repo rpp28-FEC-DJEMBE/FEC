888da467d4c6760708bfca420733a5468be067fb
Andre Boussarath <andreboussarath@gmail.com>
2021-06-09 11:09:11 -0500

    Stars rendering at 0.25 increments

--- client/reviews/Breakdown.jsx
+++ client/reviews/Breakdown.jsx
@@ -6,14 +6,45 @@ class Breakdown extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      metaData: {
+        "product_id": "22122",
+        "ratings": {
+            "2": "2",
+            "3": "3",
+            "4": "1",
+            "5": "1"
+        },
+        "recommended": {
+            "false": "1",
+            "true": "6"
+        },
+        "characteristics": {
+            "Fit": {
+                "id": 74277,
+                "value": "4.6190476190476190"
+            },
+            "Length": {
+                "id": 74278,
+                "value": "3.5000000000000000"
+            },
+            "Comfort": {
+                "id": 74279,
+                "value": "5.0000000000000000"
+            },
+            "Quality": {
+                "id": 74280,
+                "value": "4.0000000000000000"
+            }
+        }
+    }
     }
   }
 
   render() {
+    let { ratings } = this.state.metaData;
     return (
       <div className='breakdown'>
-        <RatingBreakdown />
+        <RatingBreakdown ratings={ratings}/>
         <ProductBreakdown />
       </div>
     )
--- client/reviews/RatingStars.jsx
+++ client/reviews/RatingStars.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 
-const starStyle = {
-  display: 'inline-block',
-  fontFamily: 'Times',
-  lineHeight: 1,
-}
-
-const percent= { '--percent': '50%' };
 
 const RatingStars = (props) => {
+  let starStyle = {
+    display: 'inline-block',
+    fontFamily: 'Times',
+    lineHeight: 1,
+  }
+
+  let ratingConverter = (rating) => {
+    return rating / 5 * 100;
+  }
+
+  let starPercent = ratingConverter(props.rating);
+  let percent= { '--percent': `${starPercent}%` };
 
   return (
     <div className='stars' style={{...starStyle, ...percent}}></div>
