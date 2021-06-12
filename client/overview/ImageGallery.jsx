590a97811fddf3847d2fc54c287bc27b0e5f0f35
Kingsley Allen <kingsley.allen@gmail.com>
2021-06-12 11:01:16 -0500

    Dealt with most of the errors I was getting

--- client/overview/ImageGallery.jsx
+++ client/overview/ImageGallery.jsx
@@ -3,24 +3,46 @@ import React from 'react';
 class ImageGallery extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      selectedImageIndex: 0,
+      thumbnailImageUrl: '',
+      mainImageUrl: '',
+      isLoaded: false
+    }
   }
 
-renderThumbnails() {
-  // get the style data
-  // iterate through it, urls
-  // build the ul/li fragment
+  componentDidMount() {
+    let selectedStyleId = this.props.styleId;
+    let selectedStyle = this.props.styles.find( ({style_id}) => style_id => style_id === selectedStyleId );
+    let stylePhotos = selectedStyle.photos;
+    let thumbnailImageUrl = stylePhotos[this.state.selectedImageIndex].thumbnail_url;
+    let mainImageUrl = stylePhotos[this.state.selectedImageIndex].url;
 
+    this.setState({
+      thumbnailImageUrl: thumbnailImageUrl,
+      mainImageUrl: mainImageUrl,
+      isLoaded: true
+    });
+  }
 
-}
+  renderThumbnails() {
+    // iterate through the urls
+    // map the build the ul/li fragment
+    // return the ul/li fragment
+  }
 
   render() {
 
-    if (this.props.styles === null) {
-      return null;
-    } else {
-      return(
+    if (!this.state.isLoaded) {
+      return (
         <section className="o-image-gallery">
-          <img className="o-main-image" src={this.props.styles.results[0].photos[0].url} />
+          <p>Loading...</p>
+        </section>
+      );
+    } else {
+      return (
+        <section className="o-image-gallery">
+          <img className="o-main-image" src={this.state.mainImageUrl} />
           <nav className="o-image-gallery-thumbnails">
             <ul>
               <li>x</li>
