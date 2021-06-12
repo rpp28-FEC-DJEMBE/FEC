da2c627ae6b48af1bb293335464cd81640ef500a
Simon Si <simonsi@outlook.com>
2021-06-12 09:09:21 -0700

    Updated with disclaimers on question modal

--- client/questions/addQuestion.jsx
+++ client/questions/addQuestion.jsx
@@ -24,9 +24,11 @@ class AddQuestion extends React.Component{
             <div onClick={() => this.props.handleClose()}>X</div>
           </div>
           <label>What is your nickname (mandatory)</label>
-          <input id="q-user" type="text" maxLength="60"></input>
+          <input id="q-user" type="text" maxLength="60" placeholder="Example: jackson11!"></input>
+          <p className="disclaimer">For privacy reasons, do not use your full name or email address</p>
           <label>Your email (mandatory)</label>
-          <input id="q-email" type="text" maxLength="60"></input>
+          <input id="q-email" type="text" maxLength="60" placeholder="“Why did you like the product or not?”"></input>
+          <p className="disclaimer">For authentication reasons, you will not be emailed</p>
           <label>Your Question</label>
           <textarea id="questionInput" type="text" maxLength="1000"></textarea>
           <div className="question-footer">
