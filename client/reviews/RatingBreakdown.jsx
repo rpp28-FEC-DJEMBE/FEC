7effa70e9c0047c1d4d476beff8871dd06dbe71e
Andre Boussarath <andreboussarath@gmail.com>
2021-06-14 00:29:32 -0500

    Working on star display

--- client/reviews/RatingBreakdown.jsx
+++ client/reviews/RatingBreakdown.jsx
@@ -26,7 +26,7 @@ const RatingBreakdown = ({ratings, recommended}) => {
       <p>Rating Breakdown</p>
       <div className='rating-stars'>
         <h2>{starAverage}</h2>
-        <RatingStars rating={starAverage}/>
+        <RatingStars rating={starAverage} size={35}/>
       </div>
       <p>{recommendPct}% of reviews recommend this product</p>
       <RatingBars ratings={ratings} />
