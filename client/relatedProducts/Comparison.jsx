19006366b4593d0eacda0f619e0983d943514a7b
Huiqing Li <forqing@gmail.com>
2021-06-14 20:01:47 -0700

    added sale price and comparison modal

--- client/relatedProducts/Comparison.jsx
+++ client/relatedProducts/Comparison.jsx
@@ -2,12 +2,23 @@
 import React, {useEffect, useState} from 'react';
 import style from './Comparison.css';
 const axios = require('axios');
+const {collectFeatures, combineFeatures} = require('./featuresHelper');
 
 function Comparison (props) {
   const check="\u2713";
-  const [featuresComp, setFeaturesComp] = useState([
-    {id: 0, name: '', features: []}, {id: 0, features: []}
-  ]);
+
+  const [featuresComp, setFeaturesComp] = useState(
+    {
+      id: [],
+      name: [],
+      features: [[], []],
+      featuresCombined: {
+        value1: [],
+        value2: [],
+        featureWValue: []
+      }
+  }
+  );
 
   useEffect( () => {
     getFetures(props.productId, props.clickedId);
@@ -20,72 +31,77 @@ function Comparison (props) {
       const featuresData2 = await axios.get(`/products/${clickedId}`);
       // console.log('1', currentId, featuresData1.data.name, featuresData1.data.features)
       // console.log('2', currentId, featuresData2.data.name, featuresData2.data.features)
-      setFeaturesComp([
+
+      let features1 = collectFeatures(featuresData1.data.features);
+      let features2 = collectFeatures(featuresData2.data.features);
+      let featuresCombined = combineFeatures(features1, features2);
+
+      // console.log('features', featuresData1.data.features, featuresData2.data.features)
+      // console.log(features1, features2, featuresCombined)
+
+      setFeaturesComp(
         {
-          id: currentId,
-          name: featuresData1.data.name,
-          features: featuresData1.data.features
-        },
-        {
-          id: clickedId,
-          name: featuresData2.data.name,
-          features: featuresData2.data.features
+          id: [currentId, clickedId],
+          name: [featuresData1.data.name, featuresData2.data.name],
+          features: [featuresData1.data.features, featuresData2.data.features],
+          featuresCombined: featuresCombined
         }
-      ]);
+      );
 
     } catch (err) {
       console.log(err);
     }
 
   }
-  if (featuresComp[0].name !== '') {
-    // console.log('props:', featuresComp[0].features, featuresComp);
-  }
+
+  // if (featuresComp[0].name !== '') {
+    // console.log('props:', featuresComp.features[0], featuresComp);
+  // }
+
+
   return (
     <div className="comparison-box">
       <p className="comparing">COMPARING</p>
-      <div className="comparison-name">
-        <p>{featuresComp[0].name}</p>
-        <p>{featuresComp[1].name}</p>
+      <div className="comparison-name-line">
+        <p>{featuresComp.name[0]}</p>
+        <p>{featuresComp.name[1]}</p>
       </div>
       <div className="comparison-table">
         <div>
-          {
-            featuresComp[0].features.map( feature => (
-              <p key={feature.feature+feature.value}>{feature.value}</p>
-             ) )
-          }
+          {/* <p className="comparison-name">{featuresComp.name[0]}</p> */}
           <p className="product-id">{props.productId}</p>
-          <p>{check}</p>
-        </div>
-        <div>
           {
-            featuresComp[0].features.map( feature => (
-              <p key={feature.feature+feature.value}>{feature.feature}</p>
-             ) )
-          }
-          {
-            featuresComp[1].features.map( feature => (
-              <p key={feature.feature+feature.value}>{feature.feature}</p>
-             ) )
+            featuresComp.featuresCombined.value1.map( (value1, i) => (
+              <p key={i}>{value1}</p>
+              ) )
           }
         </div>
         <div>
+          {/* <p className="no-show comparison-name">null</p> */}
+          <p className="product-id">vs</p>
           {
-            featuresComp[0].features.slice(0, featuresComp[0].features.length - 1).map( feature => (
-              <p key={feature.feature+feature.value}>{check}</p>
-             ) )
+            featuresComp.featuresCombined.featureWValue.map( (featureValue, i) => (
+              <p key={i}>{featureValue}</p>
+              ) )
           }
+        </div>
+        <div>
+          {/* <p className="comparison-name">{featuresComp.name[1]}</p> */}
           <p className="product-id">{props.clickedId}</p>
           {
-            featuresComp[1].features.map( feature => (
-              <p key={feature.feature+feature.value}>{feature.value}</p>
-             ) )
+            featuresComp.featuresCombined.value2.map( (value2, i) => {
+            if (value2 === '') {
+              return (<p key={i} className="no-show">null</p>);
+            } else {
+              return (<p key={i} className="value2">{value2}</p>);
+            }
+            } )
           }
         </div>
       </div>
     </div>
   )
+
 }
 
 export default Comparison;
