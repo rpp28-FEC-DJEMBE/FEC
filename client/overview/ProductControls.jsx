f8d9fbf5b520a9b056ba8b30340c431a68d902be
Kingsley Allen <kingsley.allen@gmail.com>
2021-06-18 02:35:50 -0500

    Implemented componentDidUpdate for productId changes, a temporary productId indicator in the upper right of the page, fetching of product data from the api, fetch, and easier parts of productControl widget (including a currency formatter util function)

--- client/overview/ProductControls.jsx
+++ client/overview/ProductControls.jsx
@@ -1,47 +1,57 @@
 import React from 'react';
-// import './overview.css';
+import './overview.css';
+
+import utils from './overviewUtils.js';
+import RatingStars from '../reviews/RatingStars.jsx';
 
 class ProductControls extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      // not yet
+
+    if (props.style) {
+      console.log('this.props.style.original_price:', utils.toCurrency(this.props.style.original_price));
+    } else {
+      console.log('No friggin this.props.style!');
     }
+
   }
 
   render() {
-    return(
-      <section className="o-product-controls">
-        {/* <StarRating /> */}
-        <p>&#9733; &#9733; &#9733; &#9733; &#9733;</p>
+    if (!this.props) {
+      return null;
+    } else {
+      return(
+        <section className="o-product-controls">
+          <RatingStars rating={3} size={14} />
 
-        <p>Category</p>
-        <p>Expanded Product Name</p>
-        <p>Price</p>
-        <p>Style > Selected Style</p>
+          <p className="o-product-category">{this.props.product[0].category}</p>
+          <p className="o-product-name">{this.props.product[0].name}</p>
+          <p className="o-product-style-price">{utils.toCurrency(this.props.style.original_price)}</p>
+          <p className="o-product-style-name"><b>STYLE > </b>{this.props.style.name}</p>
+          <hr />
+          <ul className="o-style-selctor">
+            <li>Style 1</li>
+            <li>Style 1</li>
+          </ul>
 
-        <ul className="o-style-selctor">
-          <li>Style 1</li>
-          <li>Style 1</li>
-        </ul>
+          <select name="size" className="o-size-list">
+            <option value="">Select Size</option>
+            <option value="size1">Size XS</option>
+            <option value="size2">Size XL</option>
+          </select>
 
-        <select name="size" className="o-size-list">
-          <option value="">Select Size</option>
-          <option value="size1">Size XS</option>
-          <option value="size2">Size XL</option>
-        </select>
+          <select name="quantity" className="o-quantity-list">
+            <option value="">-</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+          </select>
 
-        <select name="quantity" className="o-quantity-list">
-          <option value="">-</option>
-          <option value="1">1</option>
-          <option value="2">2</option>
-        </select>
+          <button>Add to bag</button>
 
-        <button>Add to bag</button>
-
-        <button>&#9734;</button>
-      </section>
-    )
+          <button>&#9734;</button>
+        </section>
+      )
+    }
   }
 }
 
