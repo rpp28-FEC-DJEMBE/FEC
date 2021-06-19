d91d61c3b5827060035624df2179f73bb4f7c7c4
Simon Si <simonsi@outlook.com>
2021-06-18 23:46:09 -0700

    Split component from answer.jsx into an additional component in answerEntry.jsx and renamed answer.jsx to answerList.jsx Added Helpful and Report Put requests

--- client/questions/answersList.jsx
+++ client/questions/answersList.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import axios from 'axios';
-import ExpandAnswers from './expandAnswers.jsx'
+import ExpandAnswers from './expandAnswers.jsx';
+import AnswerEntry from './answerEntry.jsx';
 
-class Answers extends React.Component {
+class AnswersList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -49,48 +50,12 @@ class Answers extends React.Component {
     .catch((err) => console.log("Error: ", err))
   }
 
-  convertDate(date){
-    const months = {
-      "01":"January",
-      "02":"February",
-      "03":"March",
-      "04":"April",
-      "05":"May",
-      "06":"June",
-      "07":"July",
-      "08":"August",
-      "09":"September",
-      "10":"October",
-      "11":"November",
-      "12":"December"
-    }
-
-    let month = months[date.slice(5,7)];
-    let day = date.slice(8,10);
-    let year = date.slice(0,4);
-
-    return `${month} ${day}, ${year}`
-  }
 
   handleExpandAnswers() {
     this.setState({
       answers: this.state.answersList
     })
   }
-
-
-// handleReportClick(answerId){
-//   const reportedList = this.state.report
-//   console.log(reportedList)
-//   if(!reportedList.includes(answerId)){
-//     this.setState({
-//       report: this.state.report.push(answerId)
-//     })
-//     console.log(answerId)
-//   } else {
-//     console.log("Can't report again!")
-//   }
-// }
 
   render() {
     if (this.state.answers.length === 0) {
@@ -99,17 +64,7 @@ class Answers extends React.Component {
     return (
       <div className="answer-list">
         {this.state.answers.map((answer, index) =>
-        <div  className="answer" key={index}>
-          <div className="answer-display">
-            <p className="a">A:</p>
-            <p className="answer-body">{answer.body}</p>
-          </div>
-          <div className="answer-footer">
-            <p>by {answer.answerer_name}, {this.convertDate(answer.date)}</p>
-            <p>Helpful? Yes({answer.helpfulness})</p>
-            <p onClick={()=> this.handleReportClick(answer.answer_id)}>report</p>
-          </div>
-        </div>
+        <AnswerEntry answer={answer} key={index} />
         )}
         <ExpandAnswers answersList={this.state.answersList} seeMoreAnswers={this.handleExpandAnswers} />
       </div>
@@ -118,4 +73,4 @@ class Answers extends React.Component {
 
 }
 
-export default Answers;
+export default AnswersList;
