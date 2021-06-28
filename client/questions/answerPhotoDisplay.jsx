5919ad960502b9c73f15d612c43078c82acc24af
Simon Si <simonsi@outlook.com>
2021-06-28 11:29:08 -0700

    added photo display on answers

---
+++ client/questions/answerPhotoDisplay.jsx
@@ -0,0 +1,33 @@
+import { render } from 'enzyme';
+import React from 'react';
+
+class AnswerPhotoDisplay extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      photos: this.props.photos
+    }
+  }
+
+  render() {
+    console.log("This is the state: ", this.state)
+    if (this.state.photos === undefined) {
+      return null;
+    } else {
+      return (
+        <div className="img-display">
+          {this.state.photos.map((photo, i) =>
+            <img src={photo.url} key={i} />
+          )}
+        </div>
+      )
+    }
+  }
+
+
+}
+
+
+
+
+export default AnswerPhotoDisplay;
