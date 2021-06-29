d775b60d7b267d8a55fbd63da1512be43e1fb4e1
Simon Si <simonsi@outlook.com>
2021-06-28 23:35:07 -0700

    Refactored answerPhotoDisplay.jsx into a functional component

--- client/questions/answerPhotoDisplay.jsx
+++ client/questions/answerPhotoDisplay.jsx
@@ -1,30 +1,18 @@
-import { render } from 'enzyme';
 import React from 'react';
 
-class AnswerPhotoDisplay extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      photos: this.props.photos
-    };
-  };
-
-  render() {
-    if (this.state.photos === undefined) {
-      return null;
-    } else {
-      return (
-        <div className="img-display">
-          {this.state.photos.map((photo, i) =>
-            <img src={photo.url} key={i} />
-          )}
-        </div>
-      )
-    }
-  };
-};
-
-
+const AnswerPhotoDisplay = (props) => {
+  if (props.photos === undefined) {
+    return null;
+  } else {
+    return (
+      <div className="img-display">
+        {props.photos.map((photo, i) =>
+          <img src={photo.url} key={i} />
+        )}
+      </div>
+    )
+  }
+}
 
 
 export default AnswerPhotoDisplay;
