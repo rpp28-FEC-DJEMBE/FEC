4774393e3dcec3e86e7f5501936843737781601e
Andre Boussarath <andreboussarath@gmail.com>
2021-06-30 10:17:45 -0500

    Refactored SortOptions to functional component

--- client/reviews/SortOptions.jsx
+++ client/reviews/SortOptions.jsx
@@ -1,32 +1,19 @@
 import React from 'react';
 
-class SortOptions extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: 'relevance'
-    }
-    this.handleChange = this.handleChange.bind(this);
-  }
+const SortOptions = (props) => {
+  let reviewNumber = props.reviews.length;
 
-  handleChange(e) {
-    this.props.handleSort(e)
-    this.setState({ value: e.target.value });
-  }
+  return (
+    <div className='sort-options'>
+      <label>{reviewNumber} reviews, sorted by </label>
+      <select value={props.sorting} onChange={(e) => props.handleSort(e)} >
+        <option value='relevant'>relevance</option>
+        <option value='helpful'>helpful</option>
+        <option value='newest'>newest</option>
+      </select>
+    </div>
+  )
 
-  render () {
-    let reviewNumber = this.props.reviews.length;
-    return (
-      <div className='sort-options'>
-        <label>{reviewNumber} reviews, sorted by </label>
-        <select value={this.props.sorting} onChange={this.handleChange} >
-          <option value='relevant'>relevance</option>
-          <option value='helpful'>helpful</option>
-          <option value='newest'>newest</option>
-        </select>
-      </div>
-    )
-  }
 }
 
 export default SortOptions;
